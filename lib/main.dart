bd25f87deeb0f54c1bd2a184a36b2987bf360dc6
Hitesh <Hiteshkhatri910@gmail.com>
2024-07-04 17:05:11 +0530

    MediaBosster

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:media_bosster_app/utils/app_routes.dart';
+
+void main()
+{
+  runApp(MaterialApp(routes: app_routes,));
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:media_bosster_app/screen/home/view/home_screen.dart';
+
+Map<String,WidgetBuilder> app_routes = {
+  "/":(context) => HomeScreen(),
+};
